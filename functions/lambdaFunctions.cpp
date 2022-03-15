962a437e81e729361f271dd1bdc3805c156749d7
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-03-15 15:48:23 +0100

    more on lambda fxns

--- functions/lambdaFunctions.cpp
+++ functions/lambdaFunctions.cpp
@@ -2,7 +2,16 @@
 #include <iostream>
 using namespace std;
 
+// we give lambda functions a possibility of calling it by assigning it to a varible 
+ auto result = [](double a , double b)->double{
+  // return (a + b);
+
+  cout<<"a + b "<< (a + b) << endl;
+};
+
+// result();
+
 
 int main(){
-
+  result(59.75 , 12.25);
 }
